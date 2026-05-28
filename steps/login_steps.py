82580daa78e275290000fc2ec09adf2ae18b169a
shub946 <shub5873@gmail.com>
2026-05-28 12:51:29 +0530

    added selenium login automation

--- steps/login_steps.py
+++ steps/login_steps.py
@@ -1,9 +1,10 @@
 import time
 
 from behave import given, when, then
-from selenium.webdriver.chrome import webdriver
+#from selenium.webdriver.chrome import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
+from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 
 

d4c0c06b42d20af58931d441ee91845f7e65b8c0
shub946 <shub5873@gmail.com>
2026-05-28 13:24:52 +0530

    added selenium login automation

--- features/login.feature
+++ features/login.feature
@@ -1,6 +1,6 @@
 @XSP-69 @released4run @daily1
 Feature: Login functionality
-
+@XSP-69
 Scenario: Valid login
     Given user opens login page
     When user enters valid username and password
--- steps/login_steps.py
+++ steps/login_steps.py
@@ -1,22 +1,15 @@
 import time
-
 from behave import given, when, then
-#from selenium.webdriver.chrome import webdriver
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
-
 
 @given('user opens login page')
 def step_impl(context):
-    context.driver = webdriver.Chrome();
-    context.driver.get("https://wccqa.on24.com/webcast/login");
-    # for maximize window
-    context.driver.maximize_window();
+    context.driver = webdriver.Chrome()
+    context.driver.get("https://wccqa.on24.com/webcast/login")
+    context.driver.maximize_window()
     time.sleep(10)
     print("Login page opened")
-
 @when('user enters valid username and password')
 def step_impl(context):
     context.driver.find_element(By.ID, "username").send_keys("qauser001")
@@ -24,7 +17,6 @@ def step_impl(context):
     context.driver.find_element(By.XPATH, "//button[@type='submit']").click()
     time.sleep(10)
     print("User entered credentials")
-
 @then('user should login successfully')
-def step_impl(context):
+def step_impl():
     print("Dashboard verified")

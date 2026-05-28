c871aeb65467e483b8d56511fa44d4da40ca2afe
shub946 <shub5873@gmail.com>
2026-05-28 12:24:35 +0530

    added multiple scenarios

--- steps/login_steps.py
+++ steps/login_steps.py
@@ -1,11 +1,33 @@
+import time
+
 from behave import given, when, then
+from selenium.webdriver.chrome import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+
 
 @given('user opens login page')
 def step_impl(context):
+    options = webdriver.ChromeOptions()
+    options.add_argument("--start-maximized")
+
+    context.driver = webdriver.Chrome(
+        service=Service(ChromeDriverManager().install()),
+        options=options
+    )
+
+    context.driver.get("https://wccqa.on24.com/webcast/login")
+    time.sleep(10)
     print("Login page opened")
 
 @when('user enters valid username and password')
 def step_impl(context):
+    context.driver.find_element(By.ID, "username").send_keys("real_username")
+    context.driver.find_element(By.ID, "password").send_keys("real_password")
+    context.driver.find_element(By.ID, "loginBtn").click()
+
+    time.sleep(10)
     print("User entered credentials")
 
 @then('user should login successfully')

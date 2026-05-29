030f860cf9702f4d0d9dcb47f12d0ebd0135f250
shub946 <shub5873@gmail.com>
2026-05-29 14:05:43 +0530

    fixed duplicate behave steps

--- steps/login_steps.py
+++ steps/login_steps.py
@@ -24,23 +24,17 @@ def step_impl(context):
     print("Login successful")
     context.driver.quit()
 
-@given('user opens login page')
-def step_impl(context):
-    context.driver = webdriver.Chrome()
-    context.driver.get("https://wccqa.on24.com/webcast/login")
-    context.driver.maximize_window()
-    time.sleep(10)
-    print("Login page opened")
+
 @when('user enters invalid username and password')
 def step_impl(context):
     context.driver.find_element(By.ID, "username").send_keys("qauser001")
     context.driver.find_element(By.ID, "password").send_keys("Password")
     context.driver.find_element(By.XPATH, "//button[@type='submit']").click()
     time.sleep(10)
-    print("User entered credentials")
+    print("User entered invalid credentials")
 @then('error messag is displaying')
 def step_impl(context):
     time.sleep(5)
     assert "webcasts" in context.driver.current_url.lower()
-    print("Invalid usernae and password")
+    print("error messag displayed")
     context.driver.quit()
